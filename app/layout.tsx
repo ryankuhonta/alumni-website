347c8f559d294f802d08457fc39fe6cef9b11055
Ryan Kuhonta <phg00311@getzphil.com>
2026-09-12 07:27:03 +0800

    Fix favicon: remove duplicate, use actual org logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,7 +69,9 @@ export async function generateMetadata(): Promise<Metadata> {
       },
     },
     icons: {
-      icon: orgInfo?.logo_url || '/favicon.ico',
+      icon: [
+        { url: '/org-logo.jpg', type: 'image/jpeg' },
+      ],
     },
   }
 }
@@ -92,9 +94,6 @@ export default async function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       style={{ '--primary-color': primaryColor } as React.CSSProperties}
     >
-      <head>
-        <link rel="icon" href={orgInfo?.logo_url || "/favicon.ico"} />
-      </head>
       <body className="min-h-full flex flex-col">
         <div className="min-h-screen flex flex-col">
           <Navbar siteName={siteName} />
